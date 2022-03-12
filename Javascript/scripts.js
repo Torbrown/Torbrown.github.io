fdf87f764f43c5b945b41055979e30f7372e0b50
Torbrown <85309544+Torbrown@users.noreply.github.com>
2022-03-11 21:15:35 -0700

    bla bla

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -4,3 +4,12 @@ const navlinks = document.getElementsByClassName('navlinks')[0]
 toggle.addEventListener('click', () => {
   navlinks.classList.toggle('active')
 })
+
+// function myFunction() {
+//   var x = document.getElementById("navbar");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }

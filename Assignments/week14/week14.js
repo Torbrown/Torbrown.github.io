2857c8a82f16ff5726ae4790d141d25de9e3791c
Torbrown <85309544+Torbrown@users.noreply.github.com>
2022-04-24 16:08:52 -0700

    assignment 14

--- Assignments/week14/week14.js
+++ Assignments/week14/week14.js
@@ -1,6 +1,9 @@
+
+//Problem two
 let companyInfo = {
   "companyName": "Tech Stars",
   "website": "www.techstars.site",
+  //Problem One
   "employees": [
     {
       "name": "Sam",
@@ -25,36 +28,46 @@ let companyInfo = {
     }
   ]
 }
-
+console.log("Problem One: First Employee List ",companyInfo.employees)
+console.log("Problem Two: company JSON", companyInfo)
+//Problem Three
 function addEmployee(obj, employeeName, employeeDepartment, employeeDesignation, employeeSalary, employeeRaise) {
   let employee = {
     "name": employeeName,
     "department": employeeDepartment,
     "designation": employeeDesignation,
     "salary": employeeSalary,
-    "raise_eligible": employeeRaise
+    "raise_eligible": employeeRaise,
   };
 
   obj['employees'].push(employee); // the push() method adds a value to an object/array.
 
 }
-addEmployee(companyInfo, "Anne", "Tech", "Executive", 25600, false);
+addEmployee(companyInfo, "Anna", "Tech", "Executive", 25600, false);
 
-console.log(companyInfo.employees)
-
+console.log("Problem Three: New Employee List ",companyInfo.employees)
+//Problem Four
 let total = 0;
 companyInfo.employees.forEach(item => {
   total = total + item.salary;
 })
 
-console.log("Total: ",total)
-
+console.log("Problem Four: Total Salaries: ",total)
+//Problem Five
 for (var i = 0; i < companyInfo.employees.length; i++) {
-  if (companyInfo.employees[i].raise_elegible == true) {
+  if (companyInfo.employees[i].raise_eligible == true) {
     let currSalary = companyInfo.employees[i].salary;
     let newSalary = (currSalary * 0.10)+currSalary;
     companyInfo.employees[i].salary = newSalary;
-    companyInfo.employees[i].raise_elegible = false;
-    console.log(companyInfo.employees[i].salary)
+    companyInfo.employees[i].raise_eligible = false;
+    console.log("Problem Five: Salary Update ",companyInfo.employees[i].salary);
   }
+}
+//Problem Six
+for (var i = 0; i < companyInfo.employees.length; i++){
+  companyInfo['employees'][i]['wfh'] = false;
+  if (companyInfo.employees[i].name == 'Anna' || companyInfo.employees[i].name == 'Sam'){
+    companyInfo['employees'][i]['wfh'] = true;
+  }
+  console.log("Problem Six: ",companyInfo.employees[i].name,companyInfo['employees'][i]['wfh'])
 }
